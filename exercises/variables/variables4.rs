d48a923d2afab60a16a232dc254148dc3a1fd91b
Savannah Dippolito <dippols@amazon.com>
2020-12-01 17:46:43 -0800

    Completed  variable excercises

--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -1,9 +1,8 @@
 // variables4.rs
 // Make me compile! Execute the command `rustlings hint variables4` if you want a hint :)
 
-// I AM NOT DONE
-
 fn main() {
-    let x: i32;
+    let x: i32 = -23; // signed 32 bit integer type (range: -(2^(n - 1)) to 2^(n - 1) - 1 inclusive, where is # of bits) https://doc.rust-lang.org/book/ch03-02-data-types.html#data-types
+
     println!("Number {}", x);
 }
--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,11 +1,12 @@
 // variables5.rs
 // Make me compile! Execute the command `rustlings hint variables5` if you want a hint :)
 
-// I AM NOT DONE
 
 fn main() {
-    let number = "T-H-R-E-E";
+    let mut number = "T-H-R-E-E";
     println!("Spell a Number : {}", number);
-    number = 3;
+    let number = 3; // Used shadowing to create a new variable with the same name.
+    // This ensures we still have an immutable variable, but can still use the same name even though its a different type
+    // Note: can not use mut for this solution as you can't change types https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html#shadowing
     println!("Number plus two is : {}", number + 2);
 }
